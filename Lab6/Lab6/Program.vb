13f59884aa7b5a7d696014e9d19d8634ddddb5d4
Nathan Emmrich <180895666+NoClass1030@users.noreply.github.com>
2026-09-14 10:15:19 -0400

    Add files via upload

---
+++ Lab6/Lab6/Program.vb
@@ -0,0 +1,5 @@
+Public Class Frm Lab6
+     Private Sub btnResponse_Click(sender As Object, e As EventArgs) Handles btnRequest.Click
+        MessageBox.Show("I got better!")
+    End Sub
+End Class
